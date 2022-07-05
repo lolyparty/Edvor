6ac7a2ad8d17fafa43cdd0ead93db7abb98a7e8b
lolyparty <ololadeolayiwola01@gmail.com>
2022-07-05 23:14:33 +0100

    fixed styling

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Hurray!! First NextJs App</title>
       </Head>
