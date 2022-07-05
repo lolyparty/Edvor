cf0529e6ad8d22b760039e85eba8d12327cc0796
lolyparty <ololadeolayiwola01@gmail.com>
2022-07-05 23:10:20 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hurray!! First NextJs App</title>
+      </Head>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
